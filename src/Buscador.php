62ccc6126f82733612594118767c7bc1b09117c1
Luis <luis_americo32@hotmail.com>
2021-09-04 12:15:23 +0100

    first commit

---
+++ src/Buscador.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    private ClientInterface $httpClient;
+    private Crawler $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementoCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementoCursos as $elementoCurso) {
+            $cursos[] = $elementoCurso->textContent;
+        }
+
+        return $cursos;
+    }
+}
